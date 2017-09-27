dfbd9cf910ddf70ef10b86ef33b9369f5b81e3ef
nikkipikki <nicole.pehar@gmail.com>
2017-09-27 11:52:26 +0200

    uploaded pages

--- quiz/views.py
+++ quiz/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
+def startpage(request):
+	return render(request, "start.html")
+
+def quiz(request, quiz_number):
+	return render(request, "quiz.html")
+
+def question(request, quiz_number, question_number):
+	return render(request, "question.html")
+
+def completed(request, quiz_number):
+	return render(request, "results.html")
+
 # Create your views here.
--- quizsite/urls.py
+++ quizsite/urls.py
@@ -13,9 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
-from django.contrib import admin
+
+
+from django.urls import path
+from quiz import views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+	path("", views.startpage),
+	path("quiz/<int:quiz_number>/", views.quiz),
+	path("quiz/<int:quiz_number>/question/<int:question_number>/", views.question),
+	path("quiz/<int:quiz_number>/completed/", views.completed),
 ]
