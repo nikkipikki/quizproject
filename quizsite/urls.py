f4316d425d4f8b53c3a19e08b0922ba063ead29d
nikkipikki <nicole.pehar@gmail.com>
2017-09-28 10:55:52 +0200

    created server

--- quizsite/urls.py
+++ quizsite/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 
 
+
+from django.contrib import admin
 from django.urls import path
 from quiz import views
 
@@ -23,4 +25,5 @@ urlpatterns = [
 	path("quiz/<int:quiz_number>/", views.quiz, name="quiz_page"),
 	path("quiz/<int:quiz_number>/question/<int:question_number>/", views.question, name="question_page"),
 	path("quiz/<int:quiz_number>/completed/", views.completed, name="completed_page"),
+	path('admin/', admin.site.urls),
 ]
